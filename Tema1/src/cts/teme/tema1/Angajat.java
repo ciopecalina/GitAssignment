a101f82702d744892df3c8c199f409905d920410
BOB <BOB@BOB-LENOVO>
2020-03-14 15:54:35 +0200

    Second Commit

--- Tema1/src/cts/teme/tema1/Angajat.java
+++ Tema1/src/cts/teme/tema1/Angajat.java
@@ -1,5 +1,21 @@
 package cts.teme.tema1;
 
 public class Angajat {
-
+	
+	private String nume;
+	private String prenume;
+	private String departament;
+	private float salariu;
+	private boolean ok;
+	
+	public Angajat(String nume, String prenume, String departament, float salariu, boolean ok) {
+		super();
+		this.nume = nume;
+		this.prenume = prenume;
+		this.departament = departament;
+		this.salariu = salariu;
+		this.ok = ok;
+	}
+	
+	
 }
